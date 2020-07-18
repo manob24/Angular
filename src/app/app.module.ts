54382a07aeed936a0b522008925261962fc1d56a
wakil <wakilurislam@gmail.com>
2020-07-18 21:29:07 +0600

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,11 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [AppComponent, MenuComponent, DishdetailComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
