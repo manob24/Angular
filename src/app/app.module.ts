a0a9adcfe86604172534cc33c362fbd532fdaff5
wakil <wakilurislam@gmail.com>
2020-07-20 20:45:26 +0600

    Angular Router Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,11 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { DishService } from './services/dish.service';
 
 @NgModule({
@@ -23,6 +27,9 @@ import { DishService } from './services/dish.service';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,6 +40,7 @@ import { DishService } from './services/dish.service';
     MatButtonModule,
     FlexLayoutModule,
     MatListModule,
+    AppRoutingModule,
   ],
   providers: [DishService],
   bootstrap: [AppComponent],
